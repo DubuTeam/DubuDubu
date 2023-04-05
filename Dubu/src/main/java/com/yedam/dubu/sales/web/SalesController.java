cf0e94a6e22f8d010399c8fddd3ce63414da93a0
suna <xkq3355@naver.com>
2023-04-05 11:32:21 +0900

    2023-04-05 선아

--- Dubu/src/main/java/com/yedam/dubu/sales/web/SalesController.java
+++ Dubu/src/main/java/com/yedam/dubu/sales/web/SalesController.java
@@ -1,4 +1,4 @@
-package com.yedam.dubu.service.web;
+package com.yedam.dubu.sales.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,39 +7,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import com.yedam.dubu.sales.service.SalesService;
 
-
-
 @Controller
 public class SalesController {
 	@Autowired
 	SalesService salesService;
+	
 	//주문서 관리
-	@GetMapping("orderList")
+	@GetMapping("/orderList")
 	public String getSalesList(Model model) {		
 		return "sales/orderList";
 	}
+	
 	//제품입고등록
-	@GetMapping("salesIst")
+	@GetMapping("/salesIst")
 	public String salesIst(Model model) {		
 		return "sales/salesIst";
 	}
+	
 	//제품입고조회
-	@GetMapping("salesIstSearch")
+	@GetMapping("/salesIstSearch")
 	public String salesIstSearch(Model model) {		
 		return "sales/salesIstSearch";
 	}
+	
 	//제품출고등록
-	@GetMapping("salesOust")
+	@GetMapping("/salesOust")
 	public String salesOust(Model model) {		
 		return "sales/salesOust";
 	}
+	
 	//제품출고조회
-	@GetMapping("salesOustSearch")
+	@GetMapping("/salesOustSearch")
 	public String salesOustSearch(Model model) {		
 		return "sales/salesOustSearch";
 	}
+	
 	//완제품LOT 재고조회
-	@GetMapping("salesStc")
+	@GetMapping("/salesStc")
 	public String salesStc(Model model) {		
 		return "sales/salesStc";
 	}
