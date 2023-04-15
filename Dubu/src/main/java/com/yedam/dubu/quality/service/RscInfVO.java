df087b26237b6c6c71030db63c8a5939f3348302
John Doe <did9984@naver.com>
2023-04-15 16:37:32 +0900

    2023-04-15 PARK

---
+++ Dubu/src/main/java/com/yedam/dubu/quality/service/RscInfVO.java
@@ -0,0 +1,16 @@
+package com.yedam.dubu.quality.service;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class RscInfVO {
+
+    private String ccdDtl;
+    private int infCnt;
+    private String ccdDtlNm;
+	
+	
+	
+}
